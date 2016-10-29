63b3abe0501e31dea30079836407f02fe7673771
Thomas Fritz <coldelectrons@users.noreply.github.com>
2016-10-29 18:25:55 -0400

    ClangFormat

--- utils/output_stream.h
+++ utils/output_stream.h
@@ -29,59 +29,61 @@ using avril::TypeInfo;
 
 namespace avril {
 
-enum EndOfLine {
-  endl = 0
-};
+enum EndOfLine { endl = 0 };
 
-template<typename Output>
+template <typename Output>
 struct OutputStream {
-  static void Print(const char* string) {
-    while (*string) {
-      Output::Write(*string++);
+    static void Print( const char* string )
+    {
+        while ( *string ) {
+            Output::Write( *string++ );
+        }
     }
-  }
-  static void Print(uint8_t byte) {
-    Output::Write(byte);
-  }
-  static void Print(char byte) {
-    Output::Write(byte);
-  }
-  static void Print(uint16_t value) {
-    char buffer[TypeInfo<uint16_t>::max_size + 1];
-    Itoa(value, TypeInfo<uint16_t>::max_size + 1, buffer);
-    Print(buffer);
-  }
-  static void Print(int16_t value) {
-    char buffer[TypeInfo<int16_t>::max_size + 1];
-    Itoa(value, TypeInfo<int16_t>::max_size + 1, buffer);
-    Print(buffer);
-  }
-  static void Print(uint32_t value) {
-    char buffer[TypeInfo<uint32_t>::max_size + 1];
-    Itoa(value, TypeInfo<uint32_t>::max_size + 1, buffer);
-    Print(buffer);
-  }
-  static void Print(uint64_t value) {
-    char buffer[TypeInfo<uint64_t>::max_size + 1];
-    Itoa(value, TypeInfo<uint64_t>::max_size + 1, buffer);
-    Print(buffer);
-  }
-  static void Print(int64_t value) {
-    char buffer[TypeInfo<int64_t>::max_size + 1];
-    Itoa(value, TypeInfo<int64_t>::max_size + 1, buffer);
-    Print(buffer);
-  }
-  static void Print(EndOfLine e) {
-    Print('\r');
-    Print('\n');
-  }
-  template<class T>
-  inline OutputStream<Output>& operator<<(const T value) {
-    Print(value);
-    return *this;
-  }
+    static void Print( uint8_t byte ) { Output::Write( byte ); }
+    static void Print( char byte ) { Output::Write( byte ); }
+    static void Print( uint16_t value )
+    {
+        char buffer[TypeInfo<uint16_t>::max_size + 1];
+        Itoa( value, TypeInfo<uint16_t>::max_size + 1, buffer );
+        Print( buffer );
+    }
+    static void Print( int16_t value )
+    {
+        char buffer[TypeInfo<int16_t>::max_size + 1];
+        Itoa( value, TypeInfo<int16_t>::max_size + 1, buffer );
+        Print( buffer );
+    }
+    static void Print( uint32_t value )
+    {
+        char buffer[TypeInfo<uint32_t>::max_size + 1];
+        Itoa( value, TypeInfo<uint32_t>::max_size + 1, buffer );
+        Print( buffer );
+    }
+    static void Print( uint64_t value )
+    {
+        char buffer[TypeInfo<uint64_t>::max_size + 1];
+        Itoa( value, TypeInfo<uint64_t>::max_size + 1, buffer );
+        Print( buffer );
+    }
+    static void Print( int64_t value )
+    {
+        char buffer[TypeInfo<int64_t>::max_size + 1];
+        Itoa( value, TypeInfo<int64_t>::max_size + 1, buffer );
+        Print( buffer );
+    }
+    static void Print( EndOfLine e )
+    {
+        Print( '\r' );
+        Print( '\n' );
+    }
+    template <class T>
+    inline OutputStream<Output>& operator<<( const T value )
+    {
+        Print( value );
+        return *this;
+    }
 };
 
 }  // namespace avril
 
-#endif   // AVRIL_OUTPUT_STREAM_H_
+#endif  // AVRIL_OUTPUT_STREAM_H_
